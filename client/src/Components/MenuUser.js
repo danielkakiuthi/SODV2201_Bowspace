2876752a340f50ddf990c383812dfdf538004a44
Daniel Kakiuthi <daniel.kakiuthi@gmail.com>
2023-11-21 17:49:15 -0700

    feat: added user role display in menu user

--- client/src/Components/MenuUser.js
+++ client/src/Components/MenuUser.js
@@ -3,10 +3,14 @@ import { Link } from 'react-router-dom'
 
 
 const MenuUser = ({ loggedUser }) => {
+  var role = "";
+  if(loggedUser) {
+     role = loggedUser.isAdmin ? "Admin":"Student";
+  }
   return (
       <div className="menu-user">
         {!loggedUser && <Link to='/login'>Login</Link>}
-        {loggedUser && <Link to= {`/users/${loggedUser.id}`}>Logged in as: {loggedUser.emailUser}</Link>}
+        {loggedUser && <Link to= {`/users/${loggedUser.id}`}>Logged in as: {loggedUser.emailUser} ({role})</Link>}
         {loggedUser && <Link to="/logout">Logout</Link>}
       </div>
   );
