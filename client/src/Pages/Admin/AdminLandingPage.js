4a20cf351c588ff3b5d04a24029a0658c2c8be6b
Daniel Kakiuthi <daniel.kakiuthi@gmail.com>
2023-11-23 14:08:16 -0700

    feat: fixed table hierarchy formating

--- client/src/Pages/Admin/AdminLandingPage.js
+++ client/src/Pages/Admin/AdminLandingPage.js
@@ -10,40 +10,48 @@ const AdminLandingPage = ({users, programs, terms, courses}) => {
       <h1>Admin Landing Page</h1>
 
         <table>
-          <tr>
-            <td><h3>Manage Users</h3></td>
-          </tr>
-          <tr>
-            <td><button onClick={() => navigate('/addUser')}>Add User</button></td>
-            <td><button onClick={() => navigate('/listUsers')}>List Users</button></td>
-          </tr>
+          <tbody>
+            <tr>
+              <td><h3>Manage Users</h3></td>
+            </tr>
+            <tr>
+              <td><button onClick={() => navigate('/addUser')}>Add User</button></td>
+              <td><button onClick={() => navigate('/listUsers')}>List Users</button></td>
+            </tr>
+          </tbody>
         </table>
         <table>
-          <tr>
-            <td><h3>Manage Programs</h3></td>
-          </tr>
-          <tr>
-            <td><button onClick={() => navigate('/addProgram')}>Add Program</button></td>
-            <td><button onClick={() => navigate('/listPrograms')}>List Programs</button></td>
-          </tr>
+          <tbody>
+            <tr>
+              <td><h3>Manage Programs</h3></td>
+            </tr>
+            <tr>
+              <td><button onClick={() => navigate('/addProgram')}>Add Program</button></td>
+              <td><button onClick={() => navigate('/listPrograms')}>List Programs</button></td>
+            </tr>
+          </tbody>
         </table>
         <table>
-          <tr>
-            <td><h3>Manage Terms</h3></td>
-          </tr>
-          <tr>
-            <td><button onClick={() => navigate('/addTerm')}>Add Term</button></td>
-            <td><button onClick={() => navigate('/listTerms')}>List Terms</button></td>
-          </tr>
+          <tbody>
+            <tr>
+              <td><h3>Manage Terms</h3></td>
+            </tr>
+            <tr>
+              <td><button onClick={() => navigate('/addTerm')}>Add Term</button></td>
+              <td><button onClick={() => navigate('/listTerms')}>List Terms</button></td>
+            </tr>
+          </tbody>
         </table>
         <table>
-          <tr>
-            <td><h3>Manage Courses</h3></td>
-          </tr>
-          <tr>
-            <td><button onClick={() => navigate('/addCourse')}>Add Course</button></td>
-            <td><button onClick={() => navigate('/listCourses')}>List Courses</button></td>
-          </tr>
+          <tbody>
+            <tr>
+              <td><h3>Manage Courses</h3></td>
+            </tr>
+            <tr>
+              <td><button onClick={() => navigate('/addCourse')}>Add Course</button></td>
+              <td><button onClick={() => navigate('/listCourses')}>List Courses</button></td>
+            </tr>
+          </tbody>
         </table>
 
     </div>
--- client/src/Pages/Student/StudentLandingPage.js
+++ client/src/Pages/Student/StudentLandingPage.js
@@ -20,31 +20,31 @@ const StudentLandingPage = ({loggedUser, users, programs, terms, courses}) => {
 
         <div>
           <h3>My Information</h3>
-          <table>
-            <tr>
-              {myPrograms.map((program) => <ul key={program.id}><li>Program Name: {program.nameProgram} </li></ul>)}
-              {myTerms.map((term) => <ul key={term.id}><li>Term #: {term.nameTerm}</li><li>Start Date: {term.startTerm}</li><li>End Date: {term.endTerm}</li></ul>)}
-            </tr>
-          </table>
+              <div>{myPrograms.map((program) => <ul key={program.id}><li>Program Name: {program.nameProgram} </li></ul>)}</div>
+              <div>{myTerms.map((term) => <ul key={term.id}><li>Term #: {term.nameTerm}</li><li>Start Date: {term.startTerm}</li><li>End Date: {term.endTerm}</li></ul>)}</div>
         </div>
 
         <div>
           <h3>Resources</h3>
           <table>
-            <tr>
-              <td><button onClick={() => navigate('/viewMyCourses')}>View My Courses</button></td>
-              <td></td>
-            </tr>
+            <tbody>
+              <tr>
+                <td><button onClick={() => navigate('/viewMyCourses')}>View My Courses</button></td>
+                <td></td>
+              </tr>
+            </tbody>
           </table>
         </div>
 
         <div>
           <h3>Registration</h3>
           <table>
-            <tr>
-              <td><button onClick={() => navigate('/registerStudentForTerm')}>Register for Term</button></td>
-              <td><button onClick={() => navigate('/registerStudentForCourses')}>Register for Courses</button></td>
-            </tr>
+            <tbody>
+              <tr>
+                <td><button onClick={() => navigate('/registerStudentForTerm')}>Register for Term</button></td>
+                <td><button onClick={() => navigate('/registerStudentForCourses')}>Register for Courses</button></td>
+              </tr>
+            </tbody>
           </table>
         </div>
 
